72f0dea1e6a3cd0f5bcd6692da7df266f5db66b2
hpx <2976633013@qq.com>
2022-06-17 21:52:36 +0800

    master commit2

--- src/main/java/com/iss/cloud/disk/model/GitTest.java
+++ src/main/java/com/iss/cloud/disk/model/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("first git");
         System.out.println("hot fix");
         System.out.println("hot fix");
+        System.out.println("master");
     }
 }
