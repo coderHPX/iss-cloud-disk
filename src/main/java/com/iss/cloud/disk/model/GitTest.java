50135ae5b0bcdb182bb7b133424f45001da5a3de
hpx <2976633013@qq.com>
2022-06-17 17:24:56 +0800

    hot commit

--- src/main/java/com/iss/cloud/disk/model/GitTest.java
+++ src/main/java/com/iss/cloud/disk/model/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("first git");
         System.out.println("first git");
+        System.out.println("hot fix");
+        System.out.println("hot fix");
     }
 }
